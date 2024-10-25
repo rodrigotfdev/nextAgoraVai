d39300b1d1e6a6aeb4d7f61b81308ec5dca5204e
Rodrigo <rtf.franca@gmail.com>
2024-10-25 12:52:25 -0300

    url fix

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -57,7 +57,7 @@ const Cart: React.FC<CartProps> = ({ isOpen, onClose }) => {
                       <span className="sr-only">Fechar painel</span>
                       <svg
                         className="h-6 w-6"
-                        xmlns="http://www.w3.org/2000/svg"
+                        xmlns="https://www.w3.org/2000/svg"
                         fill="none"
                         viewBox="0 0 24 24"
                         stroke="currentColor"
